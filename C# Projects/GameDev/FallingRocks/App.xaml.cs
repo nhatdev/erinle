3c897d4178b05bb5c9bc519138c4407309e5a4f1
adrn <adrnbeats@gmail.com>
2015-05-20 23:27:40 +0300

    sadadasad

--- C# Projects/GameDev/FallingRocks/App.xaml.cs
+++ C# Projects/GameDev/FallingRocks/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Falling_Rocks
+namespace FallingRocks
 {
     /// <summary>
     /// Interaction logic for App.xaml
